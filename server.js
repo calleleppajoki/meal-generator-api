339f9faba80fa5ccc4b97f47a47590a7f851a32c
calle leppäjoki <calle.leppajoki@gmail.com>
2017-10-11 14:08:33 +0200

    chore: read-only

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+
+// Filter by req.method
+server.all('*', function (req, res, next) {
+  if (req.method === 'GET') {
+    next() // Continue
+  } else {
+    res.sendStatus(403) // Forbidden
+  }
+})
+
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
